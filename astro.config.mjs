4641c527acf84b70d949e22141f1dc36fdb6f789
Sebastián Peña <pjfanchile@gmail.com>
2026-01-24 14:15:44 -0300

    - [feat] Se crea el componente Navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss({ content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"] })]
+  },
+
+  integrations: [react()]
+});
